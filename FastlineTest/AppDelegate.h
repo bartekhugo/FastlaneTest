90c44910c5eca7eae38c74989acc2221d0f89e4b
Bartek Hugo <bartekhugo@me.com>
2015-10-05 15:58:08 +0200

    initial commit

---
+++ FastlineTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastlineTest
+//
+//  Created by Bartek Hugo Trzcinski on 27/02/15.
+//  Copyright (c) 2015 Untitled Kingdom Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastlineTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastlineTest
+//
+//  Created by Bartek Hugo Trzcinski on 27/02/15.
+//  Copyright (c) 2015 Untitled Kingdom Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
